aa9ee7ed44b7f9f945171a96f4b9c8b1136c94cc
ravimetri <ravismetri@gmail.com>
2023-09-05 17:08:02 +0530

    phone string

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSChema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phone: { type: Number, required: true },
+    phone: { type: String, required: true },
     location: { type: String, required: false },
     isAdmin: { type: Boolean, required: false },
     isAgent: { type: Boolean, required: false },
