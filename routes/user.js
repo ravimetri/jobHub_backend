bcc946bd8f727403c63fc3766783906e7009a64e
ravimetri <ravismetri@gmail.com>
2023-09-05 17:14:58 +0530

    back errors

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const {
 
 // Update User
 
-router.put("/", verifyAndAuthorization, userController.updateUser);
+router.put("/:id", verifyAndAuthorization, userController.updateUser);
 
 //delete user
 router.delete("/:id", verifyAndAuthorization, userController.deleteUser);
