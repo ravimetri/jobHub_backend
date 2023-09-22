4ad7083e497d5cf7b4803bb383963a9c16dabe2b
ravimetri <ravismetri@gmail.com>
2023-09-22 17:52:46 +0530

    string

--- models/Bookmarks.js
+++ models/Bookmarks.js
@@ -4,7 +4,7 @@ const BookMarkSChema = new mongoose.Schema(
   {
     job: { type: mongoose.Schema.Types.ObjectId, ref: "Job" },
 
-    title: { type: mongoose.Schema, ref: "Job" },
+    title: { type: String, ref: "Job" },
     // location: { type: mongoose.Schema.Types.ObjectId, ref: "Job" },
     // company: { type: mongoose.Schema.Types.ObjectId, ref: "Job" },
     // salary: { type: mongoose.Schema.Types.ObjectId, ref: "Job" },
