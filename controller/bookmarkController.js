af839ee7d13813c362f8d4a1c91998eb03aee789
ravimetri <ravismetri@gmail.com>
2023-09-23 23:28:14 +0530

    new book

--- controller/bookmarkController.js
+++ controller/bookmarkController.js
@@ -11,12 +11,12 @@ module.exports = {
       if (!job) {
         return res.status(404).json({ error: "Job Not Found" });
       }
-      const newBook = new Bookmark({ job: job, userId: req.user.id });
+      const newBook = new BookMark({ job: job, userId: req.user.id });
 
       const savedBookmark = await newBook.save();
       const { __v, updatedAt, ...newBookmarkInfo } = savedBookmark._doc;
       // console.log(savedBookmark);
-      res.status(201).json(newBookmarkInfo);
+      res.status(200).json(newBookmarkInfo);
     } catch (error) {
       res.status(500).json(error);
     }
