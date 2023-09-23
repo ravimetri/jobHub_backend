a0af636b181ac102950bb0fcb24e830e80fa9775
ravimetri <ravismetri@gmail.com>
2023-09-23 22:22:39 +0530

    type

--- models/Bookmarks.js
+++ models/Bookmarks.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const BookMarkSChema = new mongoose.Schema(
   {
     job: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.model.ObjectId,
       ref: "Job",
 
       // ref: "Job",
