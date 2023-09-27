4f9321e13b542f8e5c4b974b41847a81077b3f71
ravimetri <ravismetri@gmail.com>
2023-09-27 22:20:57 +0530

    asa

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,4 +1,4 @@
-const Chat = require("../models/Chat");
+// const Chat = require("../models/chat");
 const User = require("../models/User");
 
 module.exports = {
--- controller/messageController.js
+++ controller/messageController.js
@@ -1,4 +1,4 @@
-const Message = require("../models/message");
+const Message = require("../models/Message");
 const User = require("../models/User");
 
 module.exports = {
