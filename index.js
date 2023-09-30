4cef51755c82e2acf7b720b77bbe19a537bfa774
ravimetri <ravismetri@gmail.com>
2023-09-30 20:54:26 +0530

    chat create

--- index.js
+++ index.js
@@ -35,7 +35,72 @@ app.use("/api/messages/", messageRoute);
 // app.listen("/api", authRoute);
 // localhost:5001/api/register
 
-app.listen(
+const server = app.listen(
   process.env.PORT || 5002,
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    //localhost
+    // origin:"http:localhost:5001"
+    origin: "https://jobhubbackend-production-4c91.up.railway.app/",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to sockets");
+
+  socket.on("setup", (userId) => {
+    socket.join(userId);
+    socket.broadcast.emit("online-user", userId);
+    console.log(userId);
+  });
+
+  socket.on("typing", (room) => {
+    console.log("typing");
+    console.log("room");
+    socket.to(room).emit("typing", room);
+  });
+
+  socket.on("stop typing", (room) => {
+    console.log("stop typing");
+    console.log("room");
+    socket.to(room).emit("stop typing", room);
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined : " + room);
+  });
+
+  socket.on("new message", (newMessageRecevied) => {
+    var chat = newMessageRecevied.chat;
+    var room = chat._id;
+
+    var sender = newMessageRecevied.sender;
+
+    if (!sender || sender._id) {
+      console.log("Sender not defined");
+      return;
+    }
+
+    var senderId = sender._id;
+    console.log(senderId + "message sender");
+    const users = chat.users;
+
+    if (!users) {
+      console.log(" User not defined ");
+      return;
+    }
+
+    socket.to(room).emit("message recevied", newMessageRecevied);
+    socket.to(room).emit("message sent", "New Message");
+  });
+
+  socket.off("setup", () => {
+    console.log("user offline");
+    socket.leave(userId);
+  });
+});
